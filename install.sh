0a23ccfec78c346c05d414f28a4f5beb0683de47
aboutorab90 <58755565+aboutorab90@users.noreply.github.com>
2023-06-29 10:05:49 +1000

    install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git scree
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+sudo apt install --fix-broken
 mkdir ~/.ssh; chmod 0700 ~/.ssh
 cat << EOF > ~/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCp+yfiO6qlgh8MdwOGfNIPwILJeo+bvK/AM/szXxtGVQHmMPoARVqAFI6PChM2wJMtTgAJIRtW5wyCQUbg54/4xzO3lzsRGvMLdWqptGQEGDrOKI9DHtxM7OOmbeN54Nl+Cdwz8yDCd8WQ9NHAE4OQM1vxFluilvaa6fivOlD1OhnnggNdKu/GqQweIopGlS7n6CXqLFyNNfXDZbpoLaqmsQqdYYHjq6HMkP1uetydNbwIJ7sbOlNh23Ote6GEauPos7SQYZfJOUqlxv4IBX/arDkxKYqL2qONKyMj5syo9GjBE/rsMdkyNsuhVWG9AC45y4hJ71CbmBzwF66NQEMkhWleI7VoP/PCe7KB4HSIFKDqZ/6SRsmorPsMA9Z4wahX+TRQnbLHAr7iGK/H+u2h+R6r8BspjXdmwS5msFtXAEv0VKbpNe4kWt3r5Dg3ly8CTN1OwL5IEwX5nX5GKAhjPNyji4Jb/fmLm5AV7Meuqv7MKDP5giljdcIodeqlpG1mQZwPbcHA5hy3oc7G+sMIX+VmsobQZ11TLHYAAOzGlRaxDScnwIHXhw/D237nfH1R/ThuQLsSAldSq8POXxzkive5ngnu6VG0N6uKw4mCF6FkPZsmK71Pi3hQITeyAqP6LP1LMlhi2ePXz8zPtikq31AVubCeWeuEYyKZns2o/Q== rehashpower@gmail.com
